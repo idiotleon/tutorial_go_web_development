58dd612442f6c5eedf5d035ddc235d47671a1fdc
Leon <yanglyu.leon.7@gmail.com>
2021-01-17 16:00:46 -0800

    tutorial: adding the interval handler
    P4

---
+++ tutorial/03/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "net/http"
+
+type helloHandler struct{}
+
+func (m *helloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World"))
+}
+
+type aboutHandler struct{}
+
+func (a *aboutHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("About!"))
+}
+
+func welcome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome!"))
+}
+
+func main() {
+	hh := helloHandler{}
+	ah := aboutHandler{}
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: nil,
+	}
+
+	http.Handle("/hello", &hh)
+	http.Handle("/about", &ah)
+
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Home!"))
+	})
+	http.HandleFunc("/welcome", welcome)
+	// http.Handle("/welcome", http.HandlerFunc(welcome))
+
+	server.ListenAndServe()
+}
